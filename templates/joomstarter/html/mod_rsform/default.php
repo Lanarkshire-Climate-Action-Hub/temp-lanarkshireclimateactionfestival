5cf9dbd3a0a01f21fddddef77dbc5a756e94d295
Russell English <russell.english@stormit.co.uk>
2025-03-01 16:34:21 +0000

    added

--- templates/joomstarter/html/mod_rsform/default.php
+++ templates/joomstarter/html/mod_rsform/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version 3.0.0
  * @package RSForm! Pro
@@ -8,7 +9,29 @@
 
 // no direct access
 defined('_JEXEC') or die;
+
+// Get module title
+$moduleTitle = $module->title;
+$headerTag = $params->get('header_tag', 'h3');
+
 ?>
-<div class="rsform uk-margin-large-left uk-margin-large-right<?php echo $moduleclass_sfx; ?>">
-	<?php echo RSFormProHelper::displayForm($formId, true); ?>
+
+<div class="uk-margin-large-bottom uk-margin-large-top uk-margin-large-right uk-margin-large-left">
+	<div uk-grid>
+		<div class="uk-width-1-2@m">
+			<div class="uk-position-relative">
+				<div class="uk-position-absolute">
+					<img src="/images/assets/person_on_bike.png" alt="person on bike" />
+				</div>
+			</div>
+		</div>
+		<div class="uk-width-1-2@m uk-margin-large-bottom uk-margin-large-top">
+			<?php if ($moduleTitle) : ?>
+				<<?php echo $headerTag; ?> class="uk-margin-bottom gardein uk-text-primary oneHundred"><span><?php echo $moduleTitle; ?></span></<?php echo $headerTag; ?>>
+			<?php endif; ?>
+			<div class="rsform" id="get-in-touch">
+				<?php echo RSFormProHelper::displayForm($formId, true); ?>
+			</div>
+		</div>
+	</div>
 </div>
